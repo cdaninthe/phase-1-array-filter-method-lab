1776ca89649f4b0ca20993582ec03fc1d950b179
Cecilia Daninthe <cecilia.daninthe@gmail.com>
2022-01-07 15:16:45 -0500

    commit tree f4c9115dd93496e83e78a6db91837adf440aa124

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(drivers, name){
+    const driverList = drivers.filter(driverName => driverName === name || driverName.toLowerCase() === name.toLowerCase())
+    return driverList;
+}
+
+function fuzzyMatch(drivers, letters){
+    const driverList = drivers.filter(driverName => driverName.startsWith(letters))
+    //const driverList = drivers.filter(driverName => driverName.slice(0, letters.length) === letters)
+    return driverList;
+}
+
+function matchName(drivers, name){
+    //const driverList = drivers.filter(driverName => driverName.matchName(name))
+    const driverList = drivers.filter(driver => driver.name === name)
+    return driverList;
+}
+
